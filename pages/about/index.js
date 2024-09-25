b1ee7816844bac5b20cba7bf7a3871c255dc0bf5
Sachintha <125460323+sachinthapramod@users.noreply.github.com>
2024-09-25 15:55:46 +0530

    1:40

--- pages/about/index.js
+++ pages/about/index.js
@@ -93,8 +93,139 @@ const aboutData = [
 import Avatar from '../../components/Avatar';
 import Circles from '../../components/Circles';
 
+//framer motion
+import {motion} from 'framer-motion';
+import {fadeIn} from '../../variants';
+
+//counter
+import CountUp from 'react-countup';
+
 const About = () => {
-  return <div>About</div>;
+  const [index, setIndex] = useState(0);
+  console.log(index);
+  return (
+    <div className='h-full bg-primary/30 py-32 text-center xl:text-left'>
+    <Circles/>
+    {/* Avatar img */}
+    <motion.div 
+      variants={fadeIn('right',0.2)}
+      initial='hidden'
+      animate='show'
+      exit='hidden'
+      className='hidden xl:flex absolute bottom-0 -left-[370px]'>
+      <Avatar/>
+    </motion.div>
+    <div className='container mx-auto h-full flex flex-col items-center xl:flex-row
+    gap-x-6'>
+      {/* text */}
+      <div className='flex-1 flex flex-col justify-center'>
+
+        <h2 className='h2'>
+           Captivating <span className='text-accent'>stories</span> birth magnificent designs.
+           </h2>
+        <p className='max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0'> 
+          10 years ago ,i began freelancing as a developer.since then ,i have
+           done remote work for agencies, counsulted for startups,and 
+           collaborated on digital products for business and consumer use.</p>
+      
+      {/* counters */}
+      <div className='hidden md:flex md:max-w-xl xl:max-w-none mx-auto xl:max-0 
+      mb-8'>
+        <div className='flex flex-1 xl:gap-x-6'>
+          {/* experience */}
+          <div className='relative flex-1 after:w-[1px] after:h-full 
+          after:bg-white/10 after:absolute after:top-0 after-right-0'>
+            <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
+              <CountUp start={0} end={10} duration={5}/> +
+            </div>
+            <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]'>
+              years of experience
+              </div>
+          </div>
+            {/* clients */}
+            <div className='relative flex-1 after:w-[1px] after:h-full 
+          after:bg-white/10 after:absolute after:top-0 after-right-0'>
+            <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
+              <CountUp start={0} end={150} duration={5}/> +
+            </div>
+            <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]'>
+              finished projects
+              </div>
+          </div>
+
+          {/* finished projects */}
+
+            <div className='relative flex-1 after:w-[1px] after:h-full 
+          after:bg-white/10 after:absolute after:top-0 after-right-0'>
+            <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
+              <CountUp start={0} end={100} duration={5}/> +
+            </div>
+            <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]'>
+              Satisfied clients
+              </div>
+          </div>
+
+          {/* winning awards */}
+
+            <div className='relative flex-1 after:w-[1px] after:h-full 
+          after:bg-white/10 after:absolute after:top-0 after-right-0'>
+            <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
+              <CountUp start={0} end={10} duration={5}/> +
+            </div>
+            <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]'>
+              awards
+              </div>
+          </div>
+
+        </div>
+      </div>
+     </div> 
+        {/* info */}
+      <div className='flex flex-col w-full xl:max-w-[48%] h-[480px]'>
+          <div className='flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4'>
+            {
+              aboutData.map((item, itemIndex) => {
+                return (
+                  <div
+                    key={itemIndex}
+                    className={`${index === itemIndex && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'} cursor-pointer capitalize xl:text-lg relative after:w-8
+                    after:h-[2px] after:bg-white after:absolute after:-bottom-1
+                    after:left-0`}
+                    onClick={() => setIndex(itemIndex)}
+                  >
+                    {item.title}
+                  </div>
+                );
+              })}
+          </div>
+          <div className='py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4
+          items-center xl:items-start'>
+            {aboutData[index].info.map((item, itemIndex)=> {
+              return (
+              <div key={itemIndex} 
+              className='flex-1 flex flex-col md:flex-row max-w-max gap-x-2
+              items-center text-white/60'
+              >
+
+                {/* title */}
+                <div className='font-light mb-2 md:mb-0'>{item.title}</div>
+                <div className='hidden md:flex'>-</div>
+                <div>{item.stage}</div>
+                <div className='flex gap-x-4'>
+                     {/* icons */}
+                {item.icons?.map((icon, itemIndex) =>{
+                  return<div className='text-2xl text-white'>{icon}</div>
+                })}
+                </div>
+               
+                </div>
+              );
+            })}
+          </div>
+      </div>
+    </div>
+  </div>
+  );
 };
 
 export default About;
