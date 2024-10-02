50b2428577a8a991e586f3230386718fe0a593a4
Sachintha <125460323+sachinthapramod@users.noreply.github.com>
2024-10-03 01:25:23 +0530

    completed(only have update 4tos & details)

--- components/Nav.js
+++ components/Nav.js
@@ -37,7 +37,7 @@ const Nav = () => {
   const pathname = router.pathname;
 
   return (
-    <nav className=" flex flex-col items-center xl:justify-center gap-y-4 fixed bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen">
+    <nav className=" flex flex-col items-center xl:justify-center gap-y-4 fixed bottom-0 mt-auto xl:right-[2%] z-50 w-full xl:w-16 xl:max-w-md xl:h-screen">
       {/* inner */}
       <div className="flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-[80px] xl:h-auto py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full">
 
@@ -52,7 +52,7 @@ const Nav = () => {
             {/*tooltip*/}
               <div className='absolute pr-14 right-0 hidden xl:group-hover:flex'>
                 <div className='bg-white flex text-primary items-center p-[6px] rounded-[3px]'>
-                    <div className='text-[12px] leading-none font-semibokld capitalize'> 
+                    <div className='text-[12px] leading-none font-semibold capitalize'> 
                       {link.name}
                       </div>
                       {/* triangle */}
